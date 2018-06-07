1643f48b1475e95c9131c25268640f82dc113ebc
GuoZhenghao <947997167@qq.com>
2018-06-07 14:40:08 +0800

    fix bug

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 export default {
 
     // guozhenghao
-    guozhenghaoServiveDomain: "http://223.223.200.50:9009",
+    guozhenghaoServiveDomain: "http://ip:port",
 
 };
--- src/utils/mapStyle.js
+++ src/utils/mapStyle.js
@@ -1,12 +1,12 @@
 export default {
 
-    world_CreamColor: "http://223.223.200.50:9200/styles/world_CreamColor/style.json",
-    chinawhite: "http://223.223.200.50:9200/styles/chinawhite/style.json",
-    chinablack: "http://223.223.200.50:9200/styles/chinablack/style.json",
-    chinablue: "http://223.223.200.50:9200/styles/chinablue/style.json",
+    world_CreamColor: "http://ip:port/styles/world_CreamColor/style.json",
+    chinawhite: "http://ip:port/styles/chinawhite/style.json",
+    chinablack: "http://ip:port/styles/chinablack/style.json",
+    chinablue: "http://ip:port/styles/chinablue/style.json",
 
-    beijingblack: "http://223.223.200.50:9200/styles/beijing_black/style.json",
-    beijingblue: "http://223.223.200.50:9200/styles/beijing_blue/style.json",
+    beijingblack: "http://ip:port/styles/beijing_black/style.json",
+    beijingblue: "http://ip:port/styles/beijing_blue/style.json",
 
 
 };
